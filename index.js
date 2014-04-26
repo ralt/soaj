9ccd3e660d66305fc5b2cd1a28450657215a822e
Florian Margaine <florian@margaine.com>
2014-04-26 23:20:22 +0200

    Small fixes after running the application.

--- index.js
+++ index.js
@@ -3,16 +3,20 @@
 var mod_getopt = require('posix-getopt');
 var Soaj = require('./lib/soaj');
 var parser, option, ip, port;
+var fs = require('fs');
+
+var ip = '127.0.0.1';
+var port = 2504;
 
 parser = new mod_getopt.BasicParser('l:(listen)p:(port)h(help)', process.argv);
 
 while ((option = parser.getopt()) !== undefined) {
     switch (option.option) {
         case 'l':
-            ip = option.optarg || '127.0.0.1';
+            ip = option.optarg;
             break;
         case 'p':
-            port = option.optarg || 2504;
+            port = option.optarg;
             break;
         case 'h':
             console.log('Help required');
@@ -24,6 +28,10 @@ if (parser.optind() >= process.argv.length) {
     console.error('missing required argument: routes file');
 }
 
-var file = require(process.argv[parser.optind()]);
-
-(new Soaj(file)).listen(port, ip);
+fs.readFile(process.argv[parser.optind()], 'utf-8', function(err, data) {
+    if (err) throw err;
+    var routes = JSON.parse(data);
+    (new Soaj(routes)).listen(port, ip, function() {
+        console.log('Listening on ' + ip + ':' + port);
+    });
+});
